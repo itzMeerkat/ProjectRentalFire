054bd4c94c3b6c3baf216b969d1f654f25d95038
JiaYi Zhang <bbfag3@gmail.com>
2020-04-03 01:33:19 -0700

    rbac tested

--- modules/rbac/rbac.py
+++ modules/rbac/rbac.py
@@ -2,29 +2,8 @@ import casbin
 
 from modules.db.firebaseapi import auth
 from modules.utils.exceptions import make_401_exception
-e = casbin.Enforcer("./rbac_model.conf", "./rbac_policy.csv")
-
-sub = "alice"  # the user that wants to access a resource.
-obj = "data1"  # the resource that is going to be accessed.
-act = "write"  # the operation that the user performs on the resource.
-
-if e.enforce(sub, obj, act):
-    print("Gut")
-else:
-    print("Nein")
-
-def checkPermission(sub, obj, act):
-    return e.enforce(sub, obj, act)
-
-
-def is_valid_token(id_token):
-    if id_token == "DEBUGTOKEN":
-        return 'SUPERUSER'
-    try:
-        decoded_token = auth.verify_id_token(id_token)
-    except:
-        raise make_401_exception("Can't verify token", "Firebase")
-    return decoded_token['uid']
+e = casbin.Enforcer("./modules/rbac/rbac_model.conf",
+                    "./modules/rbac/rbac_policy.csv")
 
 class AuthorizationFactory:
     def __init__(self, obj: str, act: str):
@@ -32,16 +11,18 @@ class AuthorizationFactory:
         self.act = act
 
     def __call__(self, token: str):
+        if token == 'DEBUG':
+            return 'su'
         try:
             decoded_token = auth.verify_id_token(token)
             uid = decoded_token['uid']
-            user = auth.get_user(uid)
-            role = user.custom_claims.get('role')
-            r = e.enforce(role, self.obj, self.act)
-            if r:
-                return uid
-        except:
-            pass
+            role = 'user'
+            if 'role' in decoded_token:
+                role = decoded_token['role']
 
-        raise make_401_exception("Invalid token or not enough permission", "Cowculator")
-        
+            r = e.enforce(role, self.obj, self.act)
+            if not r:
+                raise make_401_exception("Not enough permission", "Cowculator")
+        except:
+            raise make_401_exception("Invalid token", "Cowculator")
+        return uid
--- routers/business.py
+++ routers/business.py
@@ -3,41 +3,41 @@ from fastapi import Depends
 
 from time import time
 
-#import routers.infra.databaseapi as db
 from modules.schema.data_schema import *
 
 import modules.db.firebaseapi as db
-from modules.db.firebaseapi import is_valid_token
+from modules.rbac.rbac import AuthorizationFactory
+
 router = APIRouter()
 
 @router.get("/fb")
-async def read_system_status(uid=Depends(is_valid_token)):
-    db.debug_claim()
-    return {"res": uid}
+async def read_system_status(uid=Depends(AuthorizationFactory('debug','do'))):
+    return {"u r": uid}
 
 
 @router.post("/reservation/create")
-async def create_reservation(reservation: ReserveUserRequest, uid=Depends(is_valid_token)):
+async def create_reservation(reservation: ReserveUserRequest,
+                            uid=Depends(AuthorizationFactory('activity', 'create'))):
     res = db.reserve_item(uid, reservation.item_name, reservation.amount, int(time()*1000))
     return res
 
 
 @router.post("/reservation/cancel")
-async def cancel_reservation(request: ReserveUserCancel, uid=Depends(is_valid_token)):
+async def cancel_reservation(request: ReserveUserCancel, uid=Depends(AuthorizationFactory('activity', 'cancel'))):
     request.status = 'canceled'
     r = db.update_db('activities', 'reservation_cancel', uid, request.aid, request)
     return {'r': str(r)}
 
 
 @router.post("/reservation/checkout")
-async def checkout_equip(request: ReserveFrontdeskCheckout, uid=Depends(is_valid_token)):
+async def checkout_equip(request: ReserveFrontdeskCheckout, uid=Depends(AuthorizationFactory('activity','checkout'))):
     request.status = 'checked out'
     r = db.update_db('activities','equip_checkout',uid, request.aid, request)
     return {'r': str(r)}
 
 
 @router.post("/reservation/return")
-async def return_equip(request: ReserveFrontdeskReturn, uid=Depends(is_valid_token)):
+async def return_equip(request: ReserveFrontdeskReturn, uid=Depends(AuthorizationFactory('activity', 'return'))):
     request.status = 'closed'
     r = db.update_db('activities','equip_return',uid, request.aid, request)
     return {'r': str(r)}
